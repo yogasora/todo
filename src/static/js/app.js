95f2c0db8f4a78b3af15d0dcb8b310fa2535d74d
yogasora <76086842+yogasora@users.noreply.github.com>
2020-12-16 19:34:23 +0900

    Update app.js

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -5,35 +5,16 @@ function App() {
             <Row>
                 <Col md={{ offset: 3, span: 6 }}>
                     <TodoListCard />
-                    <addWaterMarker />
+                   
                 </Col>
             </Row>
+         <header>
+            <h3>Todo App</h3>
+        </header>
         </Container>
     );
 }
-function addWaterMarker(str){
 
-
-    var can = document.createElement('canvas');
-    var body = document.body;
-
-    body.appendChild(can);
-
-    can.width=400;
-    can.height=200;
-    can.style.display='none';
-
-
-    var cans = can.getContext('2d');
-    cans.rotate(-20*Math.PI/180);
-    cans.font = "16px Microsoft JhengHei"; 
-    cans.fillStyle = "rgba(17, 17, 17, 0.50)";
-    cans.textAlign = 'left'; 
-    cans.textBaseline = 'Middle';
-    cans.fillText(str,can.width/3,can.height/2);
-
-
-}
 function TodoListCard() {
     const [items, setItems] = React.useState(null);
 
